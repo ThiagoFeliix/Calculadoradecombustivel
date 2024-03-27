2c94eb17b7b653105e7e901feb23e7c19dce06ee
thhiagofelix <thhiagofelix@hotmail.com>
2024-03-27 12:12:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/calculadoradecombustivel/MainActivity2.kt
@@ -0,0 +1,39 @@
+package com.example.calculadoradecombustivel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
